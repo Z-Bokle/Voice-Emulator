eb732aac94c907f6107c29be0a9cf50ad6e66ce9
Bokle Zhu <Bokle@foxmail.com>
2022-04-15 23:38:15 +0800

    4.15

--- routes/api.js
+++ routes/api.js
@@ -14,11 +14,11 @@ router.get('/synthesizers', function (req, res, next) {
 
 //调用声音合成api
 router.post('/MockingBird',multipartyMiddleware,function(req, res, next){
-    let formData = req.body
-    let fileData = req.files.file
-    let filePath = fileData.path
-    let size = fileData.size
-    //包含text和file两个字段
+    let formData = req.body //表单字段
+    let fileData = req.files.file //文件信息
+    let filePath = fileData.path //文件路径
+    let size = fileData.size //文件大小
+    
     console.log(formData)
     console.log(filePath,size)
     fs.rename(filePath,filePath + '.wav',(err)=>{
